b998dd1a9eed8fbc852ccf780d8317dcd390aef1
Thibault <933kss@gmail.com>
2013-10-18 14:34:15 +0200

    sms checking

--- src/com/hackathon/Ict4Ag.java
+++ src/com/hackathon/Ict4Ag.java
@@ -4,21 +4,43 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.database.Cursor;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.Toast;
 import android.net.Uri;
 
 public class Ict4Ag extends Activity {
+	
+	String msg;
+	Button start;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.ict4_ag);
+		start = (Button) findViewById(R.id.start);
 		Uri sms = Uri.parse("content://sms/inbox");		
 		Cursor cur = getContentResolver().query(sms, null, null, null, null);
 		
 		if(cur.moveToNext())
 		{
-			
+			 msg = cur.getString(cur.getColumnIndex("body"));
+			 verify(msg);
 		}
+		
+	}
+	public String verify(String str)
+	{	String result = str.trim().substring(0,1);
+	
+		if(result == "1")
+		{
+			Toast.makeText(this.getBaseContext(), "ikibazo", Toast.LENGTH_SHORT).show();
+		}else if(result == "2")
+		{
+			Toast.makeText(this.getBaseContext(), "umusaruro", Toast.LENGTH_SHORT).show();
+		}
+		return result;
+		
 	}
 
 	@Override
