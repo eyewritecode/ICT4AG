0aabcab28062326e4e93ce1f6d21004d1da50ae9
Thibault <933kss@gmail.com>
2013-10-18 13:40:30 +0200

    listener n cursor set

--- src/com/hackathon/Ict4Ag.java
+++ src/com/hackathon/Ict4Ag.java
@@ -2,7 +2,9 @@ package com.hackathon;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.Cursor;
 import android.view.Menu;
+import android.net.Uri;
 
 public class Ict4Ag extends Activity {
 
@@ -10,6 +12,13 @@ public class Ict4Ag extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ict4_ag);
+		Uri sms = Uri.parse("content://sms/inbox");		
+		Cursor cur = getContentResolver().query(sms, null, null, null, null);
+		
+		if(cur.moveToNext())
+		{
+			
+		}
 	}
 
 	@Override
